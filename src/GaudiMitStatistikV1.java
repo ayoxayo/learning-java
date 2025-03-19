23b049fb8d51672d9bb186d5a61b18f7cff233bd
ayoxayo <ayoxayo.j704t@simplelogin.co>
2025-03-19 08:45:04 +0100

    cleaned up stuff

--- src/GaudiMitStatistikV1.java
+++ src/GaudiMitStatistikV1.java
@@ -1,9 +1,9 @@
 public class GaudiMitStatistikV1 {
     public static void main(String[] args) {
 
-        String content = "Fuck you, c#nt 123!!";
-        char[] contentArray =content.toCharArray();
-        
+        String content = "Test sentece, blah blah, had to clean up some #$@#@#%@^%^$&%^U vulgar words!! 1234 test - test...";
+        char[] contentArray = content.toCharArray();
+
         int uppercase = 0;
         int lowercase = 0;
         int number = 0;
@@ -11,7 +11,7 @@ public class GaudiMitStatistikV1 {
 
         for (int i = 0; i < contentArray.length; i++) {
             char c = content.charAt(i);
-            if ((c >= 'A') && (c<= 'Z')) // they are redundant; the () I mean
+            if ((c >= 'A') && (c <= 'Z')) // they are redundant; the () I mean
                 uppercase++;
             else if (c >= 'a' && c <= 'z')
                 lowercase++;
@@ -21,6 +21,7 @@ public class GaudiMitStatistikV1 {
                 specialChar++;
         }
 
-        System.out.println("Content: " + content + "\n" + "Uppercase: " + uppercase + "\n" + "Lowercase: " + lowercase + "\n" + "Numbers: " + number + "\n" + "Other Characters: " + specialChar);
+        System.out.println("Content: " + content + "\n" + "Uppercase: " + uppercase + "\n" + "Lowercase: " + lowercase
+                + "\n" + "Numbers: " + number + "\n" + "Other Characters: " + specialChar);
     }
 }
--- src/StellDichVorV1.java
+++ src/StellDichVorV1.java
@@ -1,16 +1,17 @@
 public class StellDichVorV1 {
     public static void main(String[] args) {
-        String name = "Igor";
-        String family_name = "Blas";
-        int age = 22;
+        String name = "a";
+        String family_name = "x";
+        int age = 11;
         String location = "Linz";
-        char nameletter = 'r';
+        char nameletter = 'a';
         double hourlyrate = 50.0;
         boolean faith = true;
 
-        System.out.println("Mein Name ist " + name + " " + family_name + "." );
-        System.out.println("Ich bin " + age + " Jahre alt und wohne derzeit in " + location + "." );
-        System.out.println( "Der letzte Buchstabe meines Nachnamens ist " + nameletter + "." );
-        System.out.println("Nach der Ausbildung werde ich " + hourlyrate + " EURO/h verdienen, und glaube heute an mich: " + faith + ".");
+        System.out.println("Mein Name ist " + name + " " + family_name + ".");
+        System.out.println("Ich bin " + age + " Jahre alt und wohne derzeit in " + location + ".");
+        System.out.println("Der letzte Buchstabe meines Nachnamens ist " + nameletter + ".");
+        System.out.println("Nach der Ausbildung werde ich " + hourlyrate
+                + " EURO/h verdienen, und glaube heute an mich: " + faith + ".");
     }
 }
