d35a7a7352332f3c7143d3c3e4015f41f406fa9c
ayoxayo <ayoxayo.j704t@simplelogin.co>
2025-03-24 09:48:13 +0100

    replaced userGuesses with pastGuesses in level2; optimized level2 if statements, implemented Math.abs with difference int; fixed ai; fixed impossible level2; fixed win condition level2; fixed wrong user feedback level 2

--- src/ZahlenRatenV1.java
+++ src/ZahlenRatenV1.java
@@ -19,8 +19,10 @@ public class ZahlenRatenV1 {
             int randomNumber = r.nextInt(0, 101);
             int userChance = 9;
             int userGuess = 0;
-            boolean yes = true;
-            ArrayList<Integer> userGuesses = new ArrayList<>();
+            boolean yes = true; // the boolean used when asking user whether he wants to repeat level 1, only
+                                // used in level 1
+            boolean win = false; // to break loop in level2 & level3
+            ArrayList<Integer> pastGuesses = new ArrayList<>();
 
             switch (menuOption) {
                 case 1:
@@ -50,38 +52,39 @@ public class ZahlenRatenV1 {
                     }
                     break;
                 case 2:
-                    while (userChance != 0 && yes) {
+                    while (userChance != 0 && !win) {
                         do {
                             System.out.printf("%s \n%s %d %s\n", "Guess a number between 0 and 100!", "You have",
                                     userChance,
                                     "attempts left.");
-                            System.out.printf("%s\n%s\n", "Past guesses:", userGuesses);
+                            System.out.printf("%s\n%s\n", "Past guesses:", pastGuesses);
                             userGuess = sc.nextInt();
                         } while (userGuess < 0 || userGuess > 100);
-                        userGuesses.add(userGuess);
-                        int difference = userGuess - randomNumber;
-                        if (userChance == randomNumber) {
+                        pastGuesses.add(userGuess);
+                        int difference = Math.abs(userGuess - randomNumber);
+                        if (userGuess == randomNumber) {
                             System.out.println("You guessed correct! Congratulations");
-                        } else if (difference <= 3 && difference >= -3) {
+                            win = true;
+                        } else if (difference <= 3) {
                             System.out.println("Almost there! You're 1-3 off!");
-                        } else if (difference <= 10 && difference >= -10) {
+                        } else if (difference <= 10) {
                             System.out.println("Relatively close! You're 4-10 off");
-                        } else if (difference <= 20 && difference >= -20) {
+                        } else if (difference <= 20) {
                             System.out.println("Not so far away! You're 10-20 off");
-                        } else if (difference > 20 || difference < -20) {
+                        } else {
                             System.out.println("Far away, you're more than 20 off!");
                         }
                         userChance--;
                     }
-                    System.out.println("Too bad, you lost!");
+                    if (!win) {
+                        System.out.println("Too bad, you lost!");
+                    }
                     break;
                 case 3:
-                    boolean win = false;
                     boolean turn = r.nextBoolean();
                     int guess = -1;
                     int min = 0;
                     int max = 100;
-                    ArrayList<Integer> pastGuesses = new ArrayList<>();
 
                     while (!win) {
                         if (turn) {
@@ -109,10 +112,12 @@ public class ZahlenRatenV1 {
                         }
                         if (guess > randomNumber) {
                             System.out.println("The number is smaller than the guess!");
-                            max = guess;
+                            if (guess <= max)
+                                max = guess - 1;
                         } else if (guess < randomNumber) {
                             System.out.println("The number is bigger than the guess!");
-                            min = guess;
+                            if (guess >= min)
+                                min = guess + 1;
                         }
                     }
                     break;
