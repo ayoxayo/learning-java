120ccdd3fae074e6e0cdc694447897b83a90ca9a
ayoxayo <ayoxayo.j704t@simplelogin.co>
2025-03-27 10:25:05 +0100

    finished RPGV2, waiting for code review, possbile bugs in combat system. Game over printing is sub optimal

--- src/RPGV2.java
+++ src/RPGV2.java
@@ -1,41 +1,33 @@
 import java.util.ArrayList;
-import java.util.Scanner;
 import java.util.Random;
+import java.util.Scanner;
 
 public class RPGV2 {
     public static void main(String[] args) {
-        // initializing scanner
         Scanner sc = new Scanner(System.in);
         Random r = new Random();
-        // initializing the variables
         String lines = "----------------------------";
         String[][] choices = {
-
                 // {parentID, ID, choiceTxt, resultTxt, moveToID }
 
                 { "-1", "0", "", "You are standing in a bar." },
 
                 { "0", "1", "Go into the wilderness in search of an adventure",
-                        "You have arrived in the wilderness. You encounter a monser" },
-
+                        "You have arrived in the wilderness. You encounter a monster" },
                 { "0", "2", "Have a drink and rest", "You enjoy your drink", "0" },
-
                 { "0", "3", "Impress the princess with your treasures", "You give it your all!", "0" },
 
                 { "1", "4", "Fight the monster", "", "6" },
-
                 { "1", "5", "Flee the monster", "You run back to the bar like a coward!", "0" },
 
                 { "4", "6", "", "" },
 
                 { "6", "7", "Try to harm the monster with your sword", "You throw yourself into battle!",
                         "6" },
-
                 { "6", "8", "It's time to go", "You attempt to flee, but the monster is chasing you", "0" },
 
                 { "-1", "9", "",
                         "Unfortunately, you time is up. Death is already waiting for you with a friendly grin" }, // dead
-
                 { "-1", "10", "", "You're the greatest hero of all time! Hooray!" }, // won
 
         }; // choice vars
@@ -52,13 +44,16 @@ public class RPGV2 {
         int mAttack = -1;
         int mGold = -1;
         int pGold = 0;
-
+        int pDamage = -1;
+        int mDamage = -1;
         // gameplay loop
         while (!gameover) {
-            if (currentChoice >= 9) {
-                gameover = true;
+            // "auto jumps"
+            if (choices[currentChoice].length == 5) {
+                System.out.printf("%s\n%s\n", choices[currentChoice][3], lines);
+                currentChoice = Integer.parseInt(choices[currentChoice][4]);
             }
-            // printer + adding IDs for the validator
+            // printer + validator
             System.out.printf("%s\n%s\n", choices[currentChoice][3], lines);
             for (int i = 0; i < choices.length; i++) {
                 parentID = Integer.parseInt(choices[i][0]);
@@ -69,13 +64,14 @@ public class RPGV2 {
                 }
             }
             System.out.println(lines);
-            // user input and validator
-            do {
-                System.out.println("What do you want to do next?");
-                currentChoice = sc.nextInt();
-            } while (!viableChoices.contains(currentChoice));
-            viableChoices.clear();
-            // manual skip changes and new mechanics
+            if (currentChoice != 9) {
+                do {
+                    System.out.println("What do you want to do next?");
+                    currentChoice = sc.nextInt();
+                } while (!viableChoices.contains(currentChoice));
+                viableChoices.clear();
+            }
+            // game mechanics and scenarios
             if (currentChoice == 2) {
                 if (pGold > 100) {
                     pGold -= 100;
@@ -86,8 +82,7 @@ public class RPGV2 {
                     System.out.println("You're too poor!\nGo slay a monster or something..");
                     currentChoice = 0;
                 }
-            }
-            if (currentChoice == 3) {
+            } else if (currentChoice == 3) {
                 if (pGold >= 1000 && lifeMax >= 15) {
                     System.out.println(
                             "The princess instantly falls in love with your treasures and mar1ries you on the spot.");
@@ -101,8 +96,7 @@ public class RPGV2 {
                             "more years of experience.");
                     currentChoice = 0;
                 }
-            }
-            if (currentChoice == 4) {
+            } else if (currentChoice == 4) {
                 mLife = r.nextInt(3, 21);
                 pAttack = r.nextInt(1, pLife);
                 mAttack = r.nextInt(1, mLife);
@@ -110,12 +104,10 @@ public class RPGV2 {
                 currentChoice = 6;
             }
             if (currentChoice == 6) {
-
-            }
-            if (currentChoice == 7) {
                 System.out.printf("%s %d %s %d %s", "The monster has", mLife, "HP, and you have", pLife, "HP.");
-                int pDamage = r.nextInt(1, pAttack);
-                int mDamage = r.nextInt(1, mAttack);
+            } else if (currentChoice == 7) {
+                pDamage = r.nextInt(1, pAttack);
+                mDamage = r.nextInt(1, mAttack);
                 mLife -= pDamage;
                 if (mLife > 0) {
                     pLife -= mDamage;
@@ -125,7 +117,7 @@ public class RPGV2 {
                         System.out.printf("%s %d %s %d %s\n", "You hit the monster with", pDamage,
                                 "Damage. It still has",
                                 mLife, "HP left.");
-                        System.out.printf("%s %d %s %d %s\n", "The monster hit you with", mDamage,
+                        System.out.printf("%s %d %s %d %s", "The monster hit you with", mDamage,
                                 "Damage. You still have",
                                 pLife, "HP left.");
                         currentChoice = 6;
@@ -134,43 +126,22 @@ public class RPGV2 {
                         lifeMax++;
                         System.out.printf("%s %d %s\n", "You have slayed the monster!\nYou find", mGold,
                                 "next to it's corpse. Naturally, you take it.\nYour experience has increased, so has your max HP");
-                        currentChoice = 4;
+                        currentChoice = 1;
                     }
                 } else {
                     currentChoice = 9;
                 }
-
-                // if (pLife < 1) {
-                // currentChoice = 9;
-                // } else if (mLife > 0) {
-                // pLife -= mDamage;
-                // System.out.printf("%s %d %s %d %s\n", "The monster hit you with", mDamage,
-                // "Damage. You still have",
-                // pLife, "HP left.");
-                // currentChoice = 6;
-                // } else {
-                // pGold += mGold;
-                // lifeMax++;
-                // System.out.printf("%s %d %s\n", "You have slayed the monster!\nYou find",
-                // mGold,
-                // "next to it's corpse. Naturally, you take it.\nYour experience has increased,
-                // so has your max HP");
-                // currentChoice = 4;
-                // }
-            }
-            if (currentChoice == 8) {
-                int mDamage = r.nextInt(0, mAttack);
+            } else if (currentChoice == 8) {
+                mDamage = r.nextInt(0, mAttack);
                 pLife -= mDamage;
                 System.out.printf("%s %d %s\n",
                         "The monster managed to hit you whilst you were running away.\nYou took", mDamage,
                         "damage.");
-            }
-            // "auto jumps"
-            if (choices[currentChoice].length == 5) {
-                System.out.printf("%s\n%s\n", choices[currentChoice][3], lines);
-                currentChoice = Integer.parseInt(choices[currentChoice][4]);
+            } else if (currentChoice == 9) {
+                gameover = true;
             }
         }
-        sc.close(); // for happy IDE
+        System.out.println("Thank you for playing!");
+        sc.close();
     }
 }
